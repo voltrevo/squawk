c1e9debb3db4ac90206d61c2092d33776f2a7440
Andrew Morris <voltrevo@gmail.com>
2021-07-21 17:48:46 +1000

    Set up typed bytes with websocket

---
+++ deps.ts
@@ -0,0 +1,4 @@
+export * as ws from "https://deno.land/std@0.93.0/ws/mod.ts";
+
+export * as tb from "https://raw.githubusercontent.com/voltrevo/monorepo/6c3a8bb/projects/typed-bytes/mod.ts";
+export * as rpc from "https://raw.githubusercontent.com/voltrevo/monorepo/6c3a8bb/projects/typed-bytes/src/rpc/index.ts";
