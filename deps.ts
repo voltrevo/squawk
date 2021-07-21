8e4e80212be40fdd0f6c3753c5a3edd57e0f8c12
Andrew Morris <voltrevo@gmail.com>
2021-07-21 18:07:25 +1000

    Add types

--- deps.ts
+++ deps.ts
@@ -1,4 +1,5 @@
-export * as ws from "https://deno.land/std@0.93.0/ws/mod.ts";
+export * as ws from "https://deno.land/std@0.102.0/ws/mod.ts";
+export { v4 as Uuid } from "https://deno.land/std@0.102.0/uuid/mod.ts";
 
 export * as tb from "https://raw.githubusercontent.com/voltrevo/monorepo/6c3a8bb/projects/typed-bytes/mod.ts";
 export * as rpc from "https://raw.githubusercontent.com/voltrevo/monorepo/6c3a8bb/projects/typed-bytes/src/rpc/index.ts";
---
+++ src/types.ts
@@ -0,0 +1,44 @@
+import { tb } from "../deps.ts";
+
+export const Player = tb.Object({
+  name: tb.string,
+  id: tb.string,
+});
+
+export const Proposal = tb.Object({
+  sendTime: tb.number,
+  recvTime: tb.number,
+  playerId: tb.string,
+  value: tb.string,
+});
+
+export const ProposalMsg = tb.Object({
+  sendTime: tb.number,
+  playerId: tb.string,
+  value: tb.string,
+});
+
+export const ActiveRound = tb.Object({
+  startTime: tb.number,
+  proposals: tb.Array(Proposal),
+});
+
+export const CompletedRound = tb.Object({
+  startTime: tb.number,
+  endTime: tb.number,
+  proposals: tb.Array(Proposal),
+  consensus: tb.string,
+});
+
+export const Team = tb.Object({
+  name: tb.string,
+  captain: Player,
+  members: tb.Array(Player),
+  completedRounds: tb.Array(CompletedRound),
+  activeRound: tb.Optional(ActiveRound),
+});
+
+export const Game = tb.Object({
+  players: tb.Array(Player),
+  teams: tb.Array(Team),
+});
